b088efc6ef31aa017c08238a1d0e5e47f00f9fde
scrooge <scrooge@yandex-team.ru>
2018-01-24 13:47:31 +0300

    add VConfig

---
+++ tests/src/test/java/erik/indahouse/VConfig.java
@@ -0,0 +1,13 @@
+package erik.indahouse;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({
+        "classpath:Config.properties"})
+
+public interface VConfig extends Config {
+    String LOGIN();
+    String PASSWORD();
+    @DefaultValue("42")
+    int maxThreads();
+}
