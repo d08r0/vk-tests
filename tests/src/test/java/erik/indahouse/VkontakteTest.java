300f6131cd4bbf07cf37b432b59bd32672ebf4cf
scrooge <scrooge@yandex-team.ru>
2018-01-31 22:49:24 +0300

    refactoring VkontakteTest

--- tests/src/test/java/erik/indahouse/VkontakteTest.java
+++ tests/src/test/java/erik/indahouse/VkontakteTest.java
@@ -7,22 +7,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import com.tngtech.java.junit.dataprovider.DataProvider;
 import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 import com.tngtech.java.junit.dataprovider.UseDataProvider;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import javax.rmi.CORBA.Util;
-import java.util.Random;
-
 
 import java.util.concurrent.TimeUnit;
 
-import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.After;
-import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -32,18 +23,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(DataProviderRunner.class)
 public class VkontakteTest {
 
+    public static final By ENTER_LOGIN = By.xpath("//*[@id='index_email']");
+    public static final By ENTER_PASSWORD = By.xpath("//*[@id='index_pass']");
+    public static final By SEND_LOGIN_BUTTON = By.xpath("//*[@id='index_login_button']");
+    public static final By SEND_FRIENDS_BUTTON = By.xpath("//li[@id='l_fr']");
+    public static final By SEARCH_FRIENDS = By.xpath("//input[@id='s_search']");
+    public static final By WRITE_MESSAGE_BUTTON = By.xpath("//*[@class='friends_field_act']");
+    public static final By ENTER_MESSAGE = By.xpath("//div[@id='mail_box_editable']");
+    public static final By SEND_MESSAGE_BUTTON = By.xpath("//*[@class='flat_button fl_r mail_box_send_btn']");
+    public static final By MESSAGE_BUTTON = By.xpath("//li[@id='l_msg']");
     private WebDriver driver;
 
     static String friend = "Венди гагарина";
     static String url = "http://www.vk.com";
     static VConfig cfg = ConfigFactory.create(VConfig.class);
 
-
     @DataProvider
     public static Object[] message() {
         return new Object[]{
-
-
                 "Привет",
                 "Hi",
                 "Hallo",
@@ -60,6 +57,8 @@ public class VkontakteTest {
     @Before
     public void createDriver() {
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(url);
     }
 
     @After
@@ -71,30 +70,20 @@ public class VkontakteTest {
     @UseDataProvider("message")
     public void TestTitle(String message) throws InterruptedException {
 
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get(url);
+        driver.findElement(ENTER_LOGIN).sendKeys(cfg.login());
+        driver.findElement(ENTER_PASSWORD).sendKeys(cfg.password());
+        driver.findElement(SEND_LOGIN_BUTTON).click();
 
-        //Авторизация
-        WebElement login1 = driver.findElement(By.id("index_email"));
-        login1.sendKeys(cfg.login());
-        WebElement password1 = driver.findElement(By.id("index_pass"));
-        password1.sendKeys(cfg.password());
-        driver.findElement(By.id("index_login_button")).click();
+        driver.findElement(SEND_FRIENDS_BUTTON).click();
+        driver.findElement(SEARCH_FRIENDS).sendKeys(friend);
+        driver.findElement(WRITE_MESSAGE_BUTTON).click();
 
-        //Поиск друга
-        driver.findElement(By.id("l_fr")).click();
-        driver.findElement(By.id("s_search")).sendKeys(friend);
-
-        //Отправка сообщения
-        driver.findElement(By.xpath("//*[@class='friends_field_act']")).click();
-        driver.findElement(By.xpath("//div[@id='mail_box_editable']")).sendKeys(message);
-
-
-        driver.findElement(By.xpath("//*[@class='flat_button fl_r mail_box_send_btn']")).click();
+        driver.findElement(ENTER_MESSAGE).sendKeys(message);
+        driver.findElement(SEND_MESSAGE_BUTTON).click();
 
         driver.navigate().refresh();
 
-        driver.findElement(By.xpath("//li[@id='l_msg']")).click();
+        driver.findElement(MESSAGE_BUTTON).click();
 
         assertThat(driver.findElement(By.xpath("//*[@class='nim-dialog--inner-text']")).getText())
                 .isEqualTo(message);
