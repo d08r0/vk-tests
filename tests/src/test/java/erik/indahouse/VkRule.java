9f774acca56fa1c40723b53e6d399653f573f48a
scrooge <scrooge@yandex-team.ru>
2018-04-03 16:33:39 +0300

    fix - VkontakteHETest

--- tests/src/test/java/erik/indahouse/VkRule.java
+++ tests/src/test/java/erik/indahouse/VkRule.java
@@ -1,15 +1,8 @@
 package erik.indahouse;
 
-import org.junit.Rule;
 import org.junit.rules.ExternalResource;
-import org.junit.runner.Description;
-import org.junit.runners.model.Statement;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import pages.FriendsPages;
-import pages.HomePages;
-import pages.LoginPages;
-import pages.MessagePage;
 
 import java.util.concurrent.TimeUnit;
 
@@ -28,16 +21,9 @@ public class VkRule extends ExternalResource {
 
     private WebDriver driver;
 
-//    public VkRule(WebDriver driver) {
-//        this.driver = driver;
-//    }
-
     @Override
     protected void before() {
-
         driver = new ChromeDriver();
-//        driver.get(url);
-
     }
 
     @Override
